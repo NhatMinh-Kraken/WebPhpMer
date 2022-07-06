6678bb43de1980fa836aabc2e2ef6ac8bb071485
NhatMinh-Kraken <nguyennhatminh26122001@gmail.com>
2022-07-06 14:26:59 +0700

    lan8

--- Register.php
+++ Register.php
@@ -2,108 +2,82 @@
     include("Layout/Head.php");
 ?>
 
-    <div class="backgr"></div>
-    <div class="Registration-form">
-        <header>
-            <h1 class="Register-header-h1">
-                Sign Up
-            </h1>
-            <p class="Register-header-p">
-                Fill In A Informations.
-            </p>
-        </header>
-        <form>
-            <div class="input-section username-section">
-                <input class="username-register username" type="text" placeholder="ENTER YOUR USERNAME HERE" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-person-square">
-                        <i class="bi bi-person-square"></i>
-                    </span>
-                    <span class="next-button username">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section email-section folder">
-                <input class="email-register email" type="email" placeholder="ENTER YOUR E-MAIL HERE" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="bi bi-envelope"></i>
-                    </span>
-                    <span class="next-button email">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section sdt-section folder">
-                <input class="sdt-register sdt" type="number" placeholder="Number Phone" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="fa fa-envelope-0"></i>
-                    </span>
-                    <span class="next-button sdt">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section password-section folder">
-                <input class="password-register pass" type="password" placeholder="Password" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="fa fa-envelope-0"></i>
-                    </span>
-                    <span class="next-button pass">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section conf-password-section folder">
-                <input class="conf-password-register conf-pass" type="password" placeholder="Conf-Password" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="fa fa-envelope-0"></i>
-                    </span>
-                    <span class="next-button conf-pass">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section Sex-section folder">
-                <select class="Sex-register Sex" type="text" id="Sex-register" name="Sex-register" autocomplete="off">
-                    <option value="0">Chọn GIới TÍnh</option>
-                    <option value="1">Nam</option>
-                    <option value="2">Nữ</option>
-                </select>
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="fa fa-envelope-0"></i>
-                    </span>
-                    <span class="next-button Sex">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-            <div class="input-section birthday-section folder">
-                <input class="birthday-register birthday" id="birthday" name="birthday" type="date" autocomplete="off">
-
-                <div class="animated-button">
-                    <span class="icon-paper-plane">
-                        <i class="fa fa-envelope-0"></i>
-                    </span>
-                    <span class="next-button birthday">
-                        <i class="fa fa-arrow-up"></i>
-                    </span>
-                </div>
-            </div>
-        </form>
-    </div>
-
     <?php
+    if(isset($_POST['DangKy'])){
+        $Username = $_POST['Username'];
+        $Email = $_POST['Email'];
+        $SDT = $_POST['SDT'];
+        $Password = md5($_POST['Password']) ;
+        $NgaySinh = $_POST['NgaySinh'];
+        $GioiTinh = $_POST['GioiTinh'];
+
+        $sql_dangky = mysqli_query('$mysqli',"INSERT INTO ")
+    }
+?>
+
+        <div class="Login-scroller">
+            <div class="container-login">
+                <div class="row Login-width">
+                    <div class="Login-background">
+                        <div class="Login-Card">
+                            <div class="Login-Tilte">
+                                <h3 class="Login-card-title">Register</h3>
+                                <form>
+                                    <div class="form-row">
+                                        <div class="form-grouper">
+                                            <div class="form-group">
+                                                <label>Username *</label>
+                                                <input type="text" class="form-control" name="Username">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Email *</label>
+                                                <input type="text" class="form-control" name="Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Password *</label>
+                                                <input type="password" class="form-control" name="Password">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Confirm Password *</label>
+                                                <input type="password" class="form-control" name="Confirm-Password">
+                                            </div>
+                                        </div>
+                                        <div class="form-grouper">
+                                            <div class="form-group">
+                                                <label>Số Điện Thoại *</label>
+                                                <input type="number" class="form-control" name="SDT">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Giới Tính *</label>
+                                                <select id="GioiTinh" name="GioiTinh" class="form-control">
+                                                    <option value="0">Chọn Giới Tính</option>
+                                                    <option value="1">Nam</option>
+                                                    <option value="2">Nữ</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Ngày Sinh *</label>
+                                                <input type="date" class="form-control" name="NgaySinh">
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="text-center">
+                                        <button type="submit" name="DangKy" class="btn btn-primary btn-block enter-btn">Enter</button>
+                                    </div>
+
+                                    <p class="sign-up">
+                                        Do you already have an account ?
+                                        <a href="Login.php"> Login</a>
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
     include("Layout/Head-end.php");
 ?>
